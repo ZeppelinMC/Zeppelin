b6635e22f258115e6a9875eed6e453580b5c4e59
oq <xcheez4@gmail.com>
2023-09-10 19:47:06 +0300

    add login page

---
+++ gui/cdn/scripts/login.js
@@ -0,0 +1,3 @@
+function login() {
+    alert(document.getElementById("input").value)
+}
--- gui/web.go
+++ gui/web.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/fs"
 	"net/http"
+	"os"
 	"strings"
 
 	"github.com/dynamitemc/dynamite/util"
@@ -25,7 +26,7 @@ var log logger
 type handler struct{}
 
 func (handler) Render(w http.ResponseWriter, name string, vars map[string]string) (int, error) {
-	f, err := guifs.ReadFile("pages/" + name)
+	f, err := os.ReadFile("gui/pages/" + name)
 	if err != nil {
 		return 0, err
 	}
@@ -40,7 +41,7 @@ func (handler) Render(w http.ResponseWriter, name string, vars map[string]string
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var code int
 	if strings.HasPrefix(r.RequestURI, "/cdn") {
-		file, err := guifs.ReadFile("cdn/" + strings.TrimPrefix(r.RequestURI, "/cdn/"))
+		file, err := os.ReadFile("gui/cdn/" + strings.TrimPrefix(r.RequestURI, "/cdn/"))
 		if err != nil {
 			if errors.Is(err, fs.ErrNotExist) {
 				io.WriteString(w, "Unknown file!")
@@ -56,7 +57,12 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case "/":
 		{
 			code = http.StatusOK
-			io.WriteString(w, "hello there!")
+			h.Render(w, "panel.html", nil)
+		}
+	case "/login":
+		{
+			code = http.StatusOK
+			h.Render(w, "login.html", nil)
 		}
 	default:
 		{
