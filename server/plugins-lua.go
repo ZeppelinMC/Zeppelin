b876bb98a647d787e05618a6a6424bdafe848e4a
oqammx86 <84847714+oq-x@users.noreply.github.com>
2023-09-23 22:33:21 +0300

    lua helper functions

--- server/plugins-lua.go
+++ server/plugins-lua.go
@@ -17,81 +17,130 @@ func luaCreateGlobalFunction(l *lua.State, k string, f lua.Function) {
 	l.SetGlobal(k)
 }
 
+func luaTable(l *lua.State, s [][2]interface{}) {
+	l.NewTable()
+	for _, t := range s {
+		key, ok := t[0].(string)
+		if !ok {
+			continue
+		}
+		switch f := t[1].(type) {
+		case func(state *lua.State) int:
+			{
+				luaCreateFunction(l, key, f)
+			}
+		}
+	}
+}
+
+func luaGlobalTable(l *lua.State, s [][2]interface{}) {
+	l.NewTable()
+	for _, t := range s {
+		key, ok := t[0].(string)
+		if !ok {
+			continue
+		}
+		switch f := t[1].(type) {
+		case func(state *lua.State) int:
+			{
+				luaCreateFunction(l, key, f)
+			}
+		}
+	}
+}
+
 func getLuaVM(logger logger.Logger, plugin *Plugin) *lua.State {
 	l := lua.NewState()
-	l.NewTable()
-	luaCreateFunction(l, "close", func(state *lua.State) int {
-		code := 0
-		if c, ok := state.ToInteger(1); ok {
-			code = c
-		}
-		os.Exit(code)
-		return 0
+	luaGlobalTable(l, [][2]interface{}{
+		{
+			"close",
+			func(state *lua.State) int {
+				code := 0
+				if c, ok := state.ToInteger(1); ok {
+					code = c
+				}
+				os.Exit(code)
+				return 0
+			},
+		},
 	})
-	l.NewTable()
-	luaCreateFunction(l, "info", func(state *lua.State) int {
-		text, ok := state.ToString(1)
-		if !ok {
-			return 0
-		}
-		var data []interface{}
-		for i := 2; ; i++ {
-			val := state.ToValue(i)
-			if val == nil {
-				break
-			}
-			data = append(data, val)
-		}
-		logger.Info(text, data...)
-		return 0
-	})
-	luaCreateFunction(l, "error", func(state *lua.State) int {
-		text, ok := state.ToString(1)
-		if !ok {
-			return 0
-		}
-		var data []interface{}
-		for i := 2; ; i++ {
-			val := state.ToValue(i)
-			if val == nil {
-				break
-			}
-			data = append(data, val)
-		}
-		logger.Error(text, data...)
-		return 0
-	})
-	luaCreateFunction(l, "debug", func(state *lua.State) int {
-		text, ok := state.ToString(1)
-		if !ok {
-			return 0
-		}
-		var data []interface{}
-		for i := 2; ; i++ {
-			val := state.ToValue(i)
-			if val == nil {
-				break
-			}
-			data = append(data, val)
-		}
-		logger.Debug(text, data...)
-		return 0
-	})
-	luaCreateFunction(l, "warn", func(state *lua.State) int {
-		text, ok := state.ToString(1)
-		if !ok {
-			return 0
-		}
-		var data []interface{}
-		for i := 2; ; i++ {
-			val := state.ToValue(i)
-			if val == nil {
-				break
-			}
-			data = append(data, val)
-		}
-		logger.Warn(text, data...)
-		return 0
+	luaTable(l, [][2]interface{}{
+		{
+			"info",
+			func(state *lua.State) int {
+				text, ok := state.ToString(1)
+				if !ok {
+					return 0
+				}
+				var data []interface{}
+				for i := 2; ; i++ {
+					val := state.ToValue(i)
+					if val == nil {
+						break
+					}
+					data = append(data, val)
+				}
+				logger.Info(text, data...)
+				return 0
+			},
+		},
+		{
+			"error",
+			func(state *lua.State) int {
+				text, ok := state.ToString(1)
+				if !ok {
+					return 0
+				}
+				var data []interface{}
+				for i := 2; ; i++ {
+					val := state.ToValue(i)
+					if val == nil {
+						break
+					}
+					data = append(data, val)
+				}
+				logger.Error(text, data...)
+				return 0
+			},
+		},
+		{
+			"debug",
+			func(state *lua.State) int {
+				text, ok := state.ToString(1)
+				if !ok {
+					return 0
+				}
+				var data []interface{}
+				for i := 2; ; i++ {
+					val := state.ToValue(i)
+					if val == nil {
+						break
+					}
+					data = append(data, val)
+				}
+				logger.Debug(text, data...)
+				return 0
+			},
+		},
+		{
+			"warn",
+			func(state *lua.State) int {
+				text, ok := state.ToString(1)
+				if !ok {
+					return 0
+				}
+				var data []interface{}
+				for i := 2; ; i++ {
+					val := state.ToValue(i)
+					if val == nil {
+						break
+					}
+					data = append(data, val)
+				}
+				logger.Warn(text, data...)
+				return 0
+			},
+		},
 	})
 	l.SetField(-2, "logger")
 	l.SetGlobal("server")
