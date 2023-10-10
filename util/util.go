01955cdd8a43f39e3a54db433919ccd807a39ebb
Angel <angelg2181@icloud.com>
2023-10-10 19:21:28 -0400

    Update util.go

--- util/util.go
+++ util/util.go
@@ -2,19 +2,7 @@ package util
 
 import (
 	"os"
-	"strings"
 )
-
-func AddDashesToUUID(uuid string) string {
-	str := ""
-	for i, char := range strings.Split(uuid, "") {
-		str += char
-		if i == 7 || i == 11 || i == 15 || i == 19 {
-			str += "-"
-		}
-	}
-	return str
-}
 
 func HasArg(arg string) bool {
 	for _, s := range os.Args {
@@ -23,22 +11,4 @@ func HasArg(arg string) bool {
 		}
 	}
 	return false
-}
-
-type Placeholders struct {
-	PlayerName   string
-	Message      string
-	PlayerGroup  string
-	PlayerPrefix string
-	PlayerSuffix string
-}
-
-func ParsePlaceholders(str string, placeholders Placeholders) string {
-	str = strings.ReplaceAll(str, "%player%", placeholders.PlayerName)
-	str = strings.ReplaceAll(str, "%message%", placeholders.Message)
-	str = strings.ReplaceAll(str, "%player_prefix%", placeholders.PlayerPrefix)
-	str = strings.ReplaceAll(str, "%player_suffix%", placeholders.PlayerSuffix)
-	str = strings.ReplaceAll(str, "%player_group%", placeholders.PlayerGroup)
-	str = strings.TrimSpace(str)
-	return str
 }
