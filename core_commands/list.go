daf4228128b98ac124c896746619cc7341df28dc
SsNiPeR1 <63201833+SsNiPeR1@users.noreply.github.com>
2023-10-21 22:20:43 +0200

    Improved /list command

--- core_commands/list.go
+++ core_commands/list.go
@@ -2,21 +2,24 @@ package core_commands
 
 import (
 	"fmt"
-	"github.com/dynamitemc/dynamite/server"
+
 	"github.com/dynamitemc/dynamite/server/commands"
 )
 
 var list_cmd = &commands.Command{
-	Name: "list",
+	Name:                "list",
+	RequiredPermissions: []string{"server.command.list"},
 	Execute: func(ctx commands.CommandContext) {
-
-		switch ex := ctx.Executor.(type) {
-
-		case *server.Session:
-			ctx.Reply(fmt.Sprintf("%v players online", len(ex.Server.Players)))
-
-		case *server.ConsoleExecutor:
-			ctx.Reply(fmt.Sprintf("%v players online", len(ex.Server.Players)))
+		srv := getServer(ctx.Executor)
+		players := srv.Players
+		if len(players) == 0 {
+			ctx.Reply("No players online")
+			return
 		}
+		msg := fmt.Sprintf("There are %d of a max of %d players online:", len(players), srv.Config.MaxPlayers)
+		for _, p := range players {
+			msg += fmt.Sprintf("\n - %s", p.Name())
+		}
+		ctx.Reply(msg)
 	},
 }
